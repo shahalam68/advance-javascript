5e6de2e88a685ba07f89d01485470b43ec7a9d69
A.K.M Shah Alam <a.k.mshahalam68@gmail.com>
2021-02-10 02:28:38 +0600

    true false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+// const name = "";
+//falsehy 
+//0
+//false
+//""
+//undefined
+//Nan
+//Truthy
+// "0",  ' ', [],{}
+let name =0;
+if(name || name == 0){
+    console.log("codition is true");
+}
+else{
+    console.log("condition is false");
+}
