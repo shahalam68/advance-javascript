04fe7bf0deff85518894dfa25f941d64782d84ad
A.K.M Shah Alam <a.k.mshahalam68@gmail.com>
2021-02-10 02:43:22 +0600

    null vs undefined

---
+++ nullVsUndefined.js
@@ -0,0 +1,17 @@
+let pakhi;
+
+function add(num1,num2){
+    console.log(num2 + num2);
+    return
+}
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+const premik = {name:"smart dude", phone:678678786};
+console.log(premik.gf);
+let fun = undefined;
+
+
+null
+
